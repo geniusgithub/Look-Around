f1cb7d5143718f878e3379eb3514911a5c911b7a
geniusgithub <lance.wyj@gmail.com>
2016-08-20 14:29:56 +0800

    delete library

--- app/src/main/java/com/geniusgithub/lookaround/activity/MainActivity.java
+++ app/src/main/java/com/geniusgithub/lookaround/activity/MainActivity.java
@@ -8,6 +8,8 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
@@ -65,6 +67,21 @@ public class MainActivity extends BaseActivityEx implements OnItemClickListener,
         initView();
         initData();
     }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+
+        if(keyCode == KeyEvent.KEYCODE_MENU) {
+            boolean ret = drawerLayout.isDrawerOpen(Gravity.LEFT);
+            if (!ret){
+                goSettingActivity();
+            }
+            return false;
+
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
 
     @Override
     public void onItemClick(AdapterView<?> adapter, View view, int position, long id) {
